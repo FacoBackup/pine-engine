e6e44fcd9e652e41162715c4e63d4dcb243bd2ca
facobackup <gustavomicaelbarbosa@gmail.com>
2024-08-03 10:15:06 -0300

    Init

---
+++ src/test/java/com/jengine/jengine/JengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jengine.jengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
